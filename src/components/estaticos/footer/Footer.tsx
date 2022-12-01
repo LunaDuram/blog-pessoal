adc949d94c8fd615a519a2072a2db828a0dd7264
LunaDuram <lucineiaduram@gmail.com>
2022-12-01 09:43:11 -0300

    Alteração do footer

--- src/components/estaticos/footer/Footer.tsx
+++ src/components/estaticos/footer/Footer.tsx
@@ -11,7 +11,7 @@ function Footer() {
                 <Grid alignItems="center" item xs={12}>
                     <Box style={{ backgroundColor: "#3F51B5", height: "120px" }}>
                         <Box paddingTop={1} display="flex" alignItems="center" justifyContent="center">
-                            <Typography variant="h5" align="center" gutterBottom style={{ color: "white" }}>Siga-nos nas redes sociais </Typography>
+                            <Typography variant="h5" align="center" gutterBottom style={{ color: "white" }}>Siga-me nas redes sociais </Typography>
                         </Box>
                         <Box display="flex" alignItems="center" justifyContent="center">
                             <a href="https://github.com/LunaDuram" target="_blank" rel="noopener noreferrer">
